96b734a4c0454683c4fb92d1c2371bdd48fcfa53
aminur-sarker <aminursarker2002@gmail.com>
2023-08-26 01:48:25 +0600

    button disable and enable

--- js/jsFile.js
+++ js/jsFile.js
@@ -1,4 +1,10 @@
-let totalValue =0;
+let totalValue = 0;
+
+    // Apply button disable and enable
+    document.getElementById('coupon-btn').disabled = true;
+
+// // make purchase button
+   document.getElementById('makePurchase').disabled = true;
 
 function anotherFile(target) {
     const selectedProduct = document.getElementById('selectedProductName');
@@ -8,14 +14,22 @@ function anotherFile(target) {
     selectedProduct.appendChild(li);
 
 
+
     const productPrice = target.childNodes[1].childNodes[3].childNodes[5].innerText.split(" ")[0];
-    
+
     totalValue = parseFloat(totalValue) + parseFloat(productPrice);
 
-    document.getElementById('totalPriceValue').innerText =  totalValue.toFixed(2);
-    
+    document.getElementById('totalPriceValue').innerText = totalValue.toFixed(2);
 
-
-
-
+    if (totalValue >= 200) {
+        document.getElementById("coupon-btn").disabled = false;
+    }
+    document.getElementById('makePurchase').disabled = false;
 }
+
+
+
+// const promoCode = document.getElementById('couponText').innerText;
+// if(promoCode === 'SELL200'){
+
+// }
